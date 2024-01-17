d607ac68c68350211a25b26f35e6571b36000b47
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-01-17 16:59:02 +0100

    pagination on marvel

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -62,3 +62,18 @@ p {
     flex-wrap: wrap;
 }
 </style>
+
+<!-- ------------------------------------- README ---------------------------------------
+Originally we wanted to use more slots but it happens the Marvel's Api images are built using an object
+which has the path and the extension separated, so in order to make it work we had to pass those properties
+to the child component and then build the src route here on this component.
+
+As you can see that suposse a problem cause we already have a cover image so in order to make things work
+we used the v-if v-else directive so we can check the properties image which is the one we will receive when this
+card component is called by the Rick and Morty component and show the correct image and if we do not receive it,
+it means this component is being called by the Marvel's Component ( Breaking bad originally) and we render the 
+ohter image. 
+
+Note: in order to indicate VueJs that the src of the img tag is going to use a variable, we have to put : before src
+example: <img :src="variable" alt="MyImageName">
+-->
